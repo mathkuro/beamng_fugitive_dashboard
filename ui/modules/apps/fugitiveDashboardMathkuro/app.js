42cbf03e97b187b10d456914bc5a4823feb22eec
mathkuro <mathkuro.git01@mathkuro.com>
2022-07-08 22:38:01 +0900

    [update] BeamNG.drive v0.25対応
    * v0.25形式の追跡情報に対応
    * 被害総額算出に使用していたAPIの非推奨化に伴う修正

--- ui/modules/apps/fugitiveDashboardMathkuro/app.js
+++ ui/modules/apps/fugitiveDashboardMathkuro/app.js
@@ -51,14 +51,11 @@ angular.module('beamng.apps')
       const DEFAULT_LANG = 'en';
       const SUPPORT_LANGS = ['en', 'ja'];
 
-      // 追跡情報：バウンティ・車両衝突回数・違反回数・追跡時間・ロードブロック設置回数・被害総額[$]
+      // 追跡情報：バウンティ・車両衝突回数・違反回数・追跡時間・ロードブロック設置回数・被害総額
       const RESULT_COLS = ['pursuitTime', 'bounty', 'hitCount', 'offensesCount', 'roadblocks', 'totalDamage'];
 
       // BeamNG.driveの実装上のヒートレベルは最高3まで。(v0.23時点)
       const MAX_HEAT_LEVEL = 3;
-
-      // ダメージ値を被害額ベースに換算するための値。試行してみた感じだと10万くらいがちょうどよさそう。
-      const DAMAGE_TO_VALUE = 100000;
 
       // リアルタイムの追跡情報用の変数を初期化。リアルタイムの値を格納するものなので初期値は全て0。
       var currentResult = {};
@@ -66,9 +63,6 @@ angular.module('beamng.apps')
         currentResult[col] = 0;
       }
       var lastHeatlevel = 0;
-      var lastRoadblockState = 0;
-      var roadblocks = 0;
-      let damages = {};
 
       // 追跡情報を処理するためのクラス。クラス化するほどのものではなかったかも。
       class Result {
@@ -105,13 +99,13 @@ angular.module('beamng.apps')
       }
 
       // 画面表示用に適宜整形を行う。(追跡情報は内部では数値型で持つ思想)
-      function prettify(data) {
+      function prettify(data, lang) {
         data.pursuitTime = sec2TimeStr(data.pursuitTime, 2);
         data.hitCount = data.hitCount.toLocaleString(undefined, {maximumFractionDigits: 0});
         data.offensesCount = data.offensesCount.toLocaleString(undefined, {maximumFractionDigits: 0});
         data.bounty = data.bounty.toLocaleString(undefined, {maximumFractionDigits: 0});
         data.roadblocks = data.roadblocks.toLocaleString(undefined, {maximumFractionDigits: 0});
-        data.totalDamage = data.totalDamage.toLocaleString(undefined, {maximumFractionDigits: 0});
+        data.totalDamage = lang.damageToLocalString(data.totalDamage);
         return data;
       }
 
@@ -128,15 +122,16 @@ angular.module('beamng.apps')
           currentResult.bounty = pursuitData['score'];
           currentResult.hitCount = pursuitData['hitCount'];
           currentResult.offensesCount = pursuitData['offensesCount'];
+          currentResult.roadblocks = pursuitData['roadblocks'];
 
-          // ロードブロックが設置されたら1カウント
-          if (pursuitData['roadblockState'] > 0 && lastRoadblockState == 0) {roadblocks += 1;}
-          lastRoadblockState = pursuitData['roadblockState'];
-          currentResult.roadblocks = roadblocks;
+          // 被害総額取得。計算はLua内で実施するように変更
+          bngApi.engineLua('mathkuro_crashUtils.getCurrentDamageByValue()', (damage) => {
+            currentResult.totalDamage += damage;
+          });
         }
 
-        var arrestLimit = (timers['arrest'] > 0 && timers['arrestLimit'] > 0) ? (timers['arrest'] / timers['arrestLimit'] * 100) : 0;
-        var evadeLimit = (timers['evade'] > 0 && timers['evadeLimit'] > 0) ? (timers['evade'] / timers['evadeLimit'] * 100) : 0;
+        var arrestLimit = timers['arrestValue'] * 100;
+        var evadeLimit = timers['evadeValue'] * 100;
         if (evadeLimit > arrestLimit) {
           arrestLimit = 0;
         } else {
@@ -145,6 +140,7 @@ angular.module('beamng.apps')
 
         // 逮捕判定の間は時刻等の数値が想定外の値を示すため描画を更新しない(各値の初期化タイミングが異なる為？)
         if (heatLevel > -1) {
+          let lang = getLang(langName);
           svg.getElementById('arrest-limit').setAttribute("width", arrestLimit);
           svg.getElementById('evade-limit').setAttribute("width", evadeLimit);
 
@@ -159,19 +155,14 @@ angular.module('beamng.apps')
           svg.getElementById('pursuitTime').textContent = sec2TimeStr(currentResult.pursuitTime, 2);
           svg.getElementById('hitCount').textContent = currentResult.hitCount.toLocaleString();
           svg.getElementById('bounty').textContent = currentResult.bounty.toFixed(1);
+          svg.getElementById('totalDamage').textContent = lang.damageToLocalString(currentResult.totalDamage);
 
-          // 違反内容は記録に残さないのでデータを直でOK
-          var tempOffenses = String(pursuitData['offenses']);
-          var offensesText = '';
+          // 違反内容表示用の文字列作成
+          let offensesText = '';
+          Object.keys(pursuitData['offenses']).forEach((key) => {
+            offensesText = (lang.offenses[key]) ? offensesText + ' ' + lang.offenses[key] + ' ' : offensesText;
+          });
 
-          if (typeof tempOffenses == 'string') {
-            var lang = getLang(langName);
-            for (const offense of tempOffenses.split(',')) {
-              offensesText = (lang.offenses[offense]) ? offensesText + ' ' + lang.offenses[offense] + ' ' : offensesText;
-            }
-          } else {
-            offensesText = '';
-          }
           svg.getElementById('offensesText').textContent = offensesText;
         }
 
@@ -204,7 +195,6 @@ angular.module('beamng.apps')
 
           })
 
-          roadblocks = 0;
         }
         lastHeatlevel = heatLevel;
       };
@@ -213,32 +203,16 @@ angular.module('beamng.apps')
       obj_main.on('load', function () {
         var svg = obj_main[0].contentDocument;
 
+        // luaモジュールの読み込み
+        bngApi.engineLua('extensions.loadAtRoot("lua/mathkuro/crashUtils", "mathkuro")');
+
         bngApi.engineLua('jsonReadFile(' + bngApi.serializeToLua(SETTINGS_PATH) + ')', (settings) => {
           var langName = (SUPPORT_LANGS.includes(settings.lang)) ? settings.lang : DEFAULT_LANG;
 
           scope.$on('streamsUpdate', function (event, data) {
-            bngApi.engineLua('extensions.gameplay_traffic.getPursuitData()', (pursuitData) => {
+            bngApi.engineLua('extensions.gameplay_police.getPursuitData()', (pursuitData) => {
               if (pursuitData) {draw(svg, pursuitData, langName);}
             });
-
-            // 被害総額算出。プレイヤー車両以外の全ての車両のダメージ/係数*車両価格を総計して算出
-            // プレイヤーが関わらないダメージ(スポーン時のダメージ等)も含まれる
-            bngApi.engineLua('gameplay_traffic.getTrafficData()', (trafficData) => {
-              for (const vid in trafficData) {
-                bngApi.engineLua('core_vehicles.getVehicleDetails(' + bngApi.serializeToLua(vid) + ').configs.Value', (value) => {
-                  let _tmp_damage = (trafficData[vid]['damage'] / DAMAGE_TO_VALUE) * value;
-                  if (vid in damages && _tmp_damage >= damages[vid]) {
-                    currentResult.totalDamage += (_tmp_damage - damages[vid]);
-                    damages[vid] = _tmp_damage;
-                  } else {
-                    currentResult.totalDamage += _tmp_damage;
-                    damages[vid] = _tmp_damage;
-                  }
-                });
-              }
-              svg.getElementById('totalDamage').textContent = currentResult.totalDamage.toLocaleString(undefined, { maximumFractionDigits: 0 });
-            });
-
           });
         });
       });
@@ -255,9 +229,9 @@ angular.module('beamng.apps')
               var langName = (SUPPORT_LANGS.includes(settings.lang)) ? settings.lang : DEFAULT_LANG;
               var lang = getLang(langName);
 
-              var last = prettify(new Result(settings.last).data);
-              var best = prettify(new Result(settings.best).data);
-              var total = prettify(new Result(settings.total).data);
+              var last = prettify(new Result(settings.last).data, lang);
+              var best = prettify(new Result(settings.best).data, lang);
+              var total = prettify(new Result(settings.total).data, lang);
 
               if (profileEnabled) {
                 profileEnabled = false;
@@ -312,14 +286,18 @@ angular.module('beamng.apps')
         hitCount = 'HIT COUNT';
         offensesCount = 'OFFENSES COUNT';
         roadblocks = 'ROADBLOCKS';
-        totalDamage = 'TOTAL DAMAGE[$]';
+        totalDamage = 'TOTAL DAMAGE';
 
         offenses = {speeding: 'Speeding', racing: 'Racing',
                     hitPolice: 'Hit(Police)', hitTraffic: 'Hit(Traffic)',
                     reckless: 'Reckless', intersection: 'Intersection',
-                    wrongWay: 'Wrong Way'}
+                    wrongWay: 'Wrong Way'};
+
+        damageToLocalString(damage) {
+          return '$' + damage.toLocaleString(undefined, { maximumFractionDigits: 0 });
+        };
       };
-      
+
       class EnLang extends BaseLang {};
       
       class JaLang extends BaseLang {
@@ -333,12 +311,40 @@ angular.module('beamng.apps')
         hitCount = '車両被害台数';
         offensesCount = '違反回数';
         roadblocks = 'ロードブロック';
-        totalDamage = '被害総額[$]';
+        totalDamage = '被害総額';
 
         offenses = {speeding: '速度超過', racing: '速度超過[重度]',
                     hitPolice: '公務執行妨害', hitTraffic: '当て逃げ',
                     reckless: '危険運転', intersection: '信号無視',
-                    wrongWay: '逆走'}
+                    wrongWay: '逆走'};
+        // 京の桁まで対応
+        maxDisplayDigits = 10000 * 10000 * 10000 * 10000 * 10000;
+
+        damageToLocalString(damage) {
+          let _damage = damage * 120;
+
+          if (_damage > this.maxDisplayDigits) {
+            return '￥' + (damage * 120).toLocaleString(undefined, { maximumFractionDigits: 0 });
+          } else {
+            let _str = String(Math.round(_damage));
+            let keta = ['', '万', '億', '兆', '京'];
+            let nums = _str.replace(/(\d)(?=(\d\d\d\d)+$)/g, "$1,").split(",").reverse();
+            let data = '';
+            for (let i = 0; i < nums.length; i++) {
+              if ((nums.length - i) > 2) {
+                continue;
+              }
+
+              if (!nums[i].match(/^[0]+$/)) {
+                data = nums[i].replace(/^[0]+/g, "") + keta[i] + data;
+              }
+            }
+            if (data == '') {
+              data = '0';
+            }
+            return data + '円';
+          }
+        };
       };
     }
   };
